aa532e5c4b292fee2f4c390bdb851390aeaf5ebf
FellipeCastro <fellipecastro27d@gmail.com>
2024-04-01 18:57:26 -0300

    aula 01

---
+++ server.js
@@ -0,0 +1,23 @@
+// Servidor Nativo
+
+// import { createServer } from 'node:http'
+
+// const server = createServer((request, response) => {
+//     response.write('Hello world')
+    
+//     return response.end()
+// })
+
+// server.listen(3333)
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', () => {
+    return 'Hello World'
+})
+
+server.listen({
+    port: 3333,
+})
