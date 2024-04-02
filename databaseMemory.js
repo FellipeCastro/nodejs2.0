c4cdcce6e301a2753d503fb502f8744c2937c1c2
FellipeCastro <fellipecastro27d@gmail.com>
2024-04-02 18:27:03 -0300

    aula 03

--- databaseMemory.js
+++ databaseMemory.js
@@ -3,8 +3,16 @@ import { randomUUID } from 'node:crypto'
 export class databaseMemory {
     #videos = new Map()
 
-    list(video) {
-        return this.#videos.values()
+    list() {
+        return Array.from(this.#videos.entries()).map((videoArray) => {
+            const id = videoArray[0]
+            const data = videoArray[1]
+
+            return {
+                id,
+                ...data
+            }
+        })
     }
 
     create(video) {
--- server.js
+++ server.js
@@ -17,24 +17,43 @@ const server = fastify()
 
 const database = new databaseMemory()
 
-server.post('/videos', () => {
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body
+
     database.create({
-        title: 'Video 01',
-        description: 'Este é o video 01',
-        duration: 180,
+        title,
+        description,
+        duration,
     })
+
+    return reply.status(201).send()
 })
 
-server.get('/videos', () => {
-    
+server.get('/videos', (request, reply) => {
+    const videos = database.list()
+
+    return videos
 })
 
-server.put('/videos/:id', () => {
-    
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
 })
 
-server.delete('/videos/:id', () => {
-    
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
